0cf4d78fafeef7d66085e9ca23e34b526a10016a
akashkhot12 <akashkhot03@gmail.com>
2024-04-08 11:14:33 +0530

    adding user component

--- blog/src/User.js
+++ blog/src/User.js
@@ -1,9 +1,11 @@
-import React from 'react'
 
 export default function User() {
   return (
-    <div>
+    <div className='User'>
 <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vitae, eos.</p>
 </div>
-  )
+  );
 }
+    
+
+
