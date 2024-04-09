d333277e49d6274ecf3de64c78709530ba4f98b9
akashkhot12 <akashkhot03@gmail.com>
2024-04-09 11:15:02 +0530

    adding bootstrap

--- practice/src/App.jsx
+++ practice/src/App.jsx
@@ -1,16 +1,43 @@
 import ClassCompo from "./component/ClassCompo";
-import React,{Component, component} from "react";
+import Button from "react-bootstrap/Button";
 
+import Table from 'react-bootstrap/Table';
 export default function App() {
   return (
     <>
-    <h1>Hello akash</h1>
-    <h2>hello chetana</h2>
-    <ClassCompo/>
-    <User/>
+      <h1>Hello akash</h1>
+      <h2>hello chetana</h2>
+      <ClassCompo />
+      <Button variant="primary">Primary</Button>
+      <Table striped bordered hover>
+      <thead>
+        <tr>
+          <th>#</th>
+          <th>First Name</th>
+          <th>Last Name</th>
+          <th>Username</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>1</td>
+          <td>Mark</td>
+          <td>Otto</td>
+          <td>@mdo</td>
+        </tr>
+        <tr>
+          <td>2</td>
+          <td>Jacob</td>
+          <td>Thornton</td>
+          <td>@fat</td>
+        </tr>
+        <tr>
+          <td>3</td>
+          <td colSpan={2}>Larry the Bird</td>
+          <td>@twitter</td>
+        </tr>
+      </tbody>
+    </Table>
     </>
-  )
+  );
 }
-
-
-
