f29324e797baa1dd726825af70dba2fa621d0978
akashkhot12 <akashkhot03@gmail.com>
2024-04-09 14:18:36 +0530

    using state and props

--- practice/src/App.jsx
+++ practice/src/App.jsx
@@ -1,15 +1,22 @@
-import React,{Component} from "react";
+import React,{Component, useState} from "react";
 // import BootstrapTable from './component/BootstrapTable'
 // import FunctionButton from "./component/FunctionButton";
 
 import Student from "./Student";
 
+import sampleData from "./sample.json";
+
 export default function App() {
+  let [data,resetData]=useState("Akash");
+  function randomName(){
+    resetData("Khot Akash")
+  }
   return (
     <>
       {/* <BootstrapTable/>
       <FunctionButton/> */}
-    <Student name={"Akash"}/>
+    <Student data={sampleData}/>
+    <button onClick={()=>randomName()}>Click me</button>
     </>
 
   );
--- practice/src/Student.jsx
+++ practice/src/Student.jsx
@@ -1,10 +1,21 @@
-function Student(props){
-    console.log(props.name);
-    return(
-        <div>
-            <h1>{props.name}</h1>
-        </div>
-    )
+function Student(props) {
+  console.log(props.data);
+  return (
+    <div>
+      {/* <h1>Hello {props.name}</h1> */}
+     {
+        props.data.map((item,index)=>(
+            <div className="">
+                <h1>Record - 0{index+1}</h1>
+                <ul>
+                    <li>First Name : {item.fname}</li>
+                    <li>Last Name : {item.lname}</li>
+                </ul>
+            </div>
+        ))
+     }
+    </div>
+  );
 }
 
 export default Student;
