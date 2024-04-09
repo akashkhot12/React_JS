45b79cc0234a730dc5c80499af1e19b6525ea6e8
akashkhot12 <akashkhot03@gmail.com>
2024-04-09 11:41:39 +0530

    adding interpollations method

--- practice/src/component/FunctionButton.jsx
+++ practice/src/component/FunctionButton.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import Button from 'react-bootstrap/Button';
 
 export default function FunctionButton() {
+    let name = "akash khot"
     function clickButton() {
         alert("you press the button..!")
     }
     return (
     <div>
+        <h1>{name}</h1>
         <Button onClick={()=>clickButton()} variant="warning">Warning</Button>
     </div>
   )
