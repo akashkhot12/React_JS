98fe89785b9a89e218de9ccc98302be2be21c9aa
akashkhot12 <akashkhot03@gmail.com>
2024-04-09 11:38:08 +0530

    adding events

--- practice/src/App.jsx
+++ practice/src/App.jsx
@@ -1,9 +1,11 @@
 import React,{Component} from "react";
 import BootstrapTable from './component/BootstrapTable'
+import FunctionButton from "./component/FunctionButton";
 export default function App() {
   return (
     <>
       <BootstrapTable/>
+      <FunctionButton/>
     </>
   );
 }
---
+++ practice/src/component/FunctionButton.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Button from 'react-bootstrap/Button';
+
+export default function FunctionButton() {
+    function clickButton() {
+        alert("you press the button..!")
+    }
+    return (
+    <div>
+        <Button onClick={clickButton} variant="warning">Warning</Button>
+    </div>
+  )
+}
