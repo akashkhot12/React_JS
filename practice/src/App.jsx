87cb54b05b1019527e7a41f77a6be4dd00e535da
akashkhot12 <akashkhot03@gmail.com>
2024-04-09 10:42:18 +0530

    adding class component

--- practice/src/App.jsx
+++ practice/src/App.jsx
@@ -1,11 +1,12 @@
-import Akash from "./component/NavBar";
+import ClassCompo from "./component/ClassCompo";
+
 
 export default function App() {
   return (
     <>
     <h1>Hello akash</h1>
     <h2>hello chetana</h2>
-    <Akash/>
+    <ClassCompo/>
     </>
   )
 }
