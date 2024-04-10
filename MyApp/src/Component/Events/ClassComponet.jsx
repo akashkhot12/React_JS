165d98fcc7c7bca31f5f6b9cb08ff0c51cde7e95
akashkhot12 <akashkhot03@gmail.com>
2024-04-10 13:45:22 +0530

    adding events

--- MyApp/src/Component/Events/ClassComponet.jsx
+++ MyApp/src/Component/Events/ClassComponet.jsx
@@ -3,14 +3,21 @@ import React, { Component } from 'react'
 export default class ClassComponet extends Component {
     constructor(){
         super();
-         this.state = {
-            data:"Akash khot"
+        this.state = {
+            data:0
         }
+    }
+
+    updateValue(){
+        this.setState({
+            data:this.state.data + 1
+        })
     }
   render() {
     return (
       <div>
         <h1>{this.state.data}</h1>
+        <button onClick={()=>this.updateValue()}>Adding </button>
       </div>
     )
   }
