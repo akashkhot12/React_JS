dad0da3e67b0ab9944ba8cb70d5138f98ad98c58
akashkhot12 <akashkhot03@gmail.com>
2024-04-10 12:31:58 +0530

    adding new project

---
+++ MyApp/src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import Mobile from './Component/basic'
+export default function App() {
+  return (
+    <div>
+      <Mobile/>
+    </div>
+  )
+}
---
+++ MyApp/src/Component/basic.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Mobile() {
+  return (
+    <div>
+        <h1>Samsung</h1>
+        <h1>Samsung</h1>
+    </div>
+  )
+}
