f5a34d3a62019e94a17a3e6ab223ec20961238f4
akashkhot12 <akashkhot03@gmail.com>
2024-04-12 11:15:23 +0530

    hide and show property

--- MyApp/src/Component/Events/HideAndShow.jsx
+++ MyApp/src/Component/Events/HideAndShow.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 
 export default function HideAndShow() {
-    const [status,setStatus]=useState(true);
+    const [status,setStatus]=React.useState(true);
   return (
     <div>
         {
