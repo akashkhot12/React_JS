b7eb240198369d93efaa8670f30d351fd4d8481a
akashkhot12 <akashkhot03@gmail.com>
2024-04-12 14:40:39 +0530

    adding project

--- MyApp/src/Component/Events/Login.jsx
+++ MyApp/src/Component/Events/Login.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function Login() {
+    const [user,setUser] = useState("");
+    const [password,setpassword] = useState("")
+    function getData(event){
+        
+        event.preventDefault()
+       
+    }
   return (
     <div>
         <h1>Login</h1>
+        <form onSubmit={getData}>
+        <input type="text" placeholder='Enter User Id' />
+        <br /><br />
+        <input type="text" placeholder='Enter User Password' />
+        <br /><br />
+        <button type='submit'>Login</button>
+        </form>
     </div>
   )
 }
