4239ef16c65e270a7fc1cea391040089b4089f71
akashkhot12 <akashkhot03@gmail.com>
2024-06-02 13:08:15 +0530

    adding effect hooks

--- Hooks/src/EffectHooks.jsx
+++ Hooks/src/EffectHooks.jsx
@@ -1,11 +1,17 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 export default function EffectHooks() {
+    const [content,setContent] = useState("Post");
+    const [count,setCount] = useState(0)
+
   return (
     <div>
-        <button>Post</button>
-        <button>User</button>
-        <button>Comment</button>
+        <button onClick={()=>setCount(count+1)}>{count} count</button>
+        <button onClick={()=>setContent("Post")}>Post</button>
+        <button onClick={()=>setContent("User")}>User</button>
+        <button onClick={()=>setContent("Comment")}>Comment</button>
+
+        <h2>{content}</h2>
     </div>
   )
 }
