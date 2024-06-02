735ba5153c824d3aae5a2f80269cdbd98785f681
akashkhot12 <akashkhot03@gmail.com>
2024-06-02 13:02:45 +0530

    adding effect hooks

--- Hooks/src/App.jsx
+++ Hooks/src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import StateHook from './StateHook'
 import './App.css'
+import EffectHooks from './EffectHooks'
 
 export default function App() {
   return (
     <div>
-      <StateHook/>
+      {/* <StateHook/> */}
+      <EffectHooks/>
     </div>
   )
 }
---
+++ Hooks/src/EffectHooks.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function EffectHooks() {
+  return (
+    <div>
+        <button>Post</button>
+        <button>User</button>
+        <button>Comment</button>
+    </div>
+  )
+}
