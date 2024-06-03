d3b964a294f30a6e09511713a19aa43d301ba9a6
akashkhot12 <akashkhot03@gmail.com>
2024-06-03 12:45:27 +0530

    adding new context hooks

--- Hooks/src/App.jsx
+++ Hooks/src/App.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
-import StateHook from './StateHook'
+// import StateHook from './StateHook'
 import './App.css'
-import EffectHooks from './EffectHooks'
+import E from './assets/UseContextHooks/E'
+// import EffectHooks from './EffectHooks'
+
 
 export default function App() {
   return (
     <div>
       {/* <StateHook/> */}
-      <EffectHooks/>
+      {/* <EffectHooks/> */}
+      <E/>
     </div>
   )
 }
