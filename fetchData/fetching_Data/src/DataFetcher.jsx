bbcc8e974ed001bd863a0ca4aef755bd0d10dc90
akashkhot12 <akashkhot03@gmail.com>
2024-06-06 23:07:58 +0530

    completed task

--- fetchData/fetching_Data/src/DataFetcher.jsx
+++ fetchData/fetching_Data/src/DataFetcher.jsx
@@ -37,15 +37,7 @@ export default function DataFetcher() {
 
   return (
     <div>
-        <h1>Fetch data</h1>
-        {/* <ul>
-            {data.map(item=>(
-                <li key={item.id}>{item.title}</li>
-            ))}
-        </ul> */}
-
-        
-
+        <h4>Fetch data</h4>
 <div className="relative overflow-x-auto">
     <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
         <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
