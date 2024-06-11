ad0bd1c88f9e3a691d224c29aba552882645d735
akashkhot12 <akashkhot03@gmail.com>
2024-06-11 12:12:03 +0530

    adding new hooks

--- Hooks/src/assets/Ref.jsx
+++ Hooks/src/assets/Ref.jsx
@@ -1,7 +1,18 @@
-import React from 'react'
+import React, { useRef } from 'react'
 
-export default function Ref() {
+export default function Ref() { 
+    let inputRef = useRef(null);
+    function handleInput(){
+        console.log("Function called");
+        inputRef.current.value = "10000"
+        inputRef.current.focus();
+        inputRef.current.style.color = "red";
+    }
   return (
-    <div>Ref</div>
+    <div>
+        <h1>Use ref in react</h1>
+        <input type="text" ref={inputRef} />
+        <button onClick={handleInput}>handle Input</button>
+    </div>
   )
 }
