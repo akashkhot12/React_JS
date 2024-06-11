c1c683d33097aa745864107281eae8f3ac9a34bc
akashkhot12 <akashkhot03@gmail.com>
2024-06-11 11:11:21 +0530

    complete 4 hooks

--- Hooks/src/assets/Memo.jsx
+++ Hooks/src/assets/Memo.jsx
@@ -1,17 +1,22 @@
-import React, { useState } from 'react'
+import React, { useMemo, useState } from 'react'
 
 export default function Memo() {
 const [count,setCount] = useState(0);
 const [item,setItem] = useState(0);
 
+const memorised = useMemo(function multi(){
+    console.log("multi called");
+    return count*5;
+},[count])
 
 
   return (
     <div>
-        <h1>{count}</h1>
-        <h1>{item}</h1>
-        <button>add count</button>
-        <button>add items</button>
+        <h1>count is  = {count}</h1>
+        <h1> item is = {item}</h1>
+        <h2>{memorised}</h2>
+        <button onClick={()=>setCount(count+1)}>add count</button>
+        <button onClick={()=>setItem(item+1)}>add items</button>
     </div>
   )
 }
