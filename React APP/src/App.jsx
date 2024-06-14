6910c1552ab55fc23339f7a22635b7e7d3050504
akashkhot12 <akashkhot03@gmail.com>
2024-06-14 11:43:14 +0530

    adding new project

---
+++ React APP/src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+import { Link } from 'react-router-dom'
+
+function App() {
+
+
+  return (
+    <div className="container mx-auto">
+      <nav className='bg-gary-100 flex gap-4'>
+      <Link to="/invoices"><span>Invoices</span></Link>
+      <Link to="/expenses"><span>Expenses</span></Link>
+      </nav>
+    </div>
+  )
+}
+
+export default App
